c032ce9eb8a7735dd6415d14d6fdc5f11599e69d
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-08-03 13:03:11 +0545

    feat: add vscode integration

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,11 +11,14 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    -- lang specific extras
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    -- vscode
+    { import = "lazyvim.plugins.extras.vscode" },
     -- import/override with your plugins
     { import = "plugins" },
   },
