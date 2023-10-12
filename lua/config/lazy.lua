d91c155b5fec6f51564b0000e2fca6252f1f7711
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-10-12 21:50:05 +0545

    feat: remove default formatter from conform

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     -- formatting and fixes
-    { import = "lazyvim.plugins.extras.formatting.conform" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.formatting.black" },
@@ -29,6 +28,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "plugins" },
   },
+
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custoqm plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
