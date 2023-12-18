11dff399c9cbdc2dbbd2e8e232246be502f97142
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-12-18 13:13:52 +0545

    feat: add one dark theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,7 @@
 return {
-  "JoosepAlviste/palenightfall.nvim",
-  priority = 1000,
-  opts = {},
+  "olimorris/onedarkpro.nvim",
+  priority = 1000, -- Ensure it loads first
+  config = function()
+    vim.cmd("colorscheme onedark")
+  end,
 }
