b3757af5a2daaa99debe4049787d8429f2aea276
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-01-07 14:25:20 +0545

    feat: add dadbod

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_exec(
+  [[
+    augroup db_autocmd
+        autocmd!
+        autocmd FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
+    augroup END
+]],
+  false
+)
