b6c78886e1a0131af5b5fb02b9e0a4cb7bd2b080
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-04-21 11:53:33 +0545

    feat: add justfile filetype

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.filetype.add({
+  filename = {
+    ["justfile"] = "just",
+    ["JUSTFILE"] = "just",
+    [".justfile"] = "just",
+  },
+})
