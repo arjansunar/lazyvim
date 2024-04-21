342e45144a6671ab26d54ce25b03b9cde8b10210
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-04-21 06:38:00 +0545

    feat: add scrip to get api key for open api

--- lua/plugins/chat-gpt.lua
+++ lua/plugins/chat-gpt.lua
@@ -3,8 +3,10 @@ return {
   "jackMort/ChatGPT.nvim",
   event = "VeryLazy",
   config = function()
-    -- TODO: get api keys
-    require("chatgpt").setup()
+    local home = os.getenv("HOME")
+    require("chatgpt").setup({
+      api_key_cmd = "bash " .. home .. "/get-gpt/api-key.sh",
+    })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
