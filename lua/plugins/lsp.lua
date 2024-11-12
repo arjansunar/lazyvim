43f07f78314016b501bc5e06e49c5d1bf2cfc0b8
arjansunar <arjan.gahatrajsunar@gmail.com>
2024-11-12 17:59:34 +0545

    feat: enable ruff diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,11 +4,42 @@ return {
     "neovim/nvim-lspconfig",
     {
       "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          ts_ls = { enabled = false },
-        },
-      },
+      opts = function(_, opts)
+        -- print(vim.inspect(opts.servers))
+        -- table.insert(opts.servers, { ts_ls = { enabled = false } })
+      end,
     },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "ruff",
+      })
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, {
+        require("none-ls.formatting.ruff"),
+        require("none-ls.diagnostics.ruff"),
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      vim.list_extend(opts.formatters_by_ft, {
+        ["python"] = { "ruff" },
+      })
+    end,
   },
 }
